0a0faffccb2c98a79a684e0360ebd8bcf1de5af4
wesley <wesleyyw8@gmail.com>
2018-04-13 00:51:38 +0100

    results page done

--- src/search-results/search-results.controller.js
+++ src/search-results/search-results.controller.js
@@ -1,19 +1,27 @@
 import { getCheapFlights } from 'Services/cheapflights.service';
 import { FLIGHTS_URL } from 'Enums';
+import moment from 'moment';
 
-export default function SearchResultsController($scope, $stateParams) {
+export default function SearchResultsController($scope, $stateParams, $timeout) {
   'ngInject';
-  // console.log($stateParams.arrival, $stateParams.dateArrival, $stateParams.dateDeparture, $stateParams.departure);
-  const params = {
+  $scope.params = {
     arrival: $stateParams.arrival,
     dateArrival: $stateParams.dateArrival,
     dateDeparture: $stateParams.dateDeparture,
     departure: $stateParams.departure
   }
   
-  //$scope.flightList = {A: 3};
-  getCheapFlights(params).then(data => {
-    $scope.flightList = data;
-    console.log($scope.flightList);
+  getCheapFlights($scope.params).then(data => {
+    $timeout(() => {
+      $scope.flightList = data;
+      $scope.$apply();
+    })
   });
+
+  $scope.formatHour = value => 
+    moment(new Date(value)).format('HH:MM')
+
+  $scope.formatPrice = value =>
+    Math.round(value).toFixed(2)
+  
 }
